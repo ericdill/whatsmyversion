4b5724c284c08dd4d0ef3a894988469881ade550
Eric Dill <thedizzle@gmail.com>
2015-08-27 22:33:15 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# theversion
+Single module python package to make versioning simple.  You add a couple of
+configuration parameters to your top-most python module and `theversion` will
+determine your git version string
+
+
+## Use cases
+
+1. Determine the version of a git repository
+
+    1. Installed with `setup.py develop`
+    1. Installed with `setup.py install`
+    
+1. Determine the version of source code downloaded from github
+
+
+## installation (not currently functional)
+pip install theversion
+conda install -c ericdill theversion
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+import os
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='theversion',
+    version='0.0.0',
+    author='Eric Dill',
+    description="Making versioning easy",
+    long_description=read('README.md'),
+    pymodules=['theversion'],
+    url='http://github.com/ericdill/theversion',
+    license='GPL-3',
+)
